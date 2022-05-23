52f16d43da4794b918a05e3b6a5b731868fe71d5
Rayelus <92628182+Rayelus5@users.noreply.github.com>
2022-05-23 16:46:47 +0200

    New Update!

--- assets/js/enterAccountNumber.js
+++ assets/js/enterAccountNumber.js
@@ -3,43 +3,50 @@ function btnLogin() {
         var colors001 = input001.value;
 
         if (colors001 == "0xA732zb9") {
-          text001 = "Login as Ray P.M."
+          alert("Login as ADMIN");
+          text001 = "Login as Ray P.M.";
           document.getElementById('message001').innerHTML = text001;
 
-          window.open('account/0.html')
+          window.open('account/0.html');
         }
 
         else if (colors001 == "0xA204ta7") {
-          text001 = "Login as Álvaro Poo"
+          alert("Login as Álvaro Poo");
+          text001 = "Login as Álvaro Poo";
           document.getElementById('message001').innerHTML = text001;
 
-          window.open('account/1.html')
+          window.open('account/1.html');
         }
 
         else if (colors001 == "0xB701cr3") {
-          text001 = "Login as Dulce María"
+          alert("Login as Dulce María");
+          text001 = "Login as Dulce María";
           document.getElementById('message001').innerHTML = text001;
 
-          window.open('account/2.html')
+          window.open('account/2.html');
         }
 
         else if (colors001 == "0xC509bp2") {
-          text001 = "Login as Laura Mosquero"
+          alert("Login as Laura Mosquero");
+          text001 = "Login as Laura Mosquero";
           document.getElementById('message001').innerHTML = text001;
 
-          window.open('account/3.html')
+          window.open('account/3.html');
         }
 
         else if (colors001 == "0xE325aq6") {
-          text001 = "Login as Ray P.P."
+          alert("Login as Ray P.P.");
+          text001 = "Login as Ray P.P.";
           document.getElementById('message001').innerHTML = text001;
 
-          window.open('account/4.html')
+          window.open('account/4.html');
         }
 
         else {
-          text001 = "Debe escribir su número de cuenta"
+          alert("Debe escribir su número de cuenta");
+          text001 = "Debe escribir su número de cuenta";
           document.getElementById('message001').innerHTML = text001;
+          window('index.html');
         }
         
       }
